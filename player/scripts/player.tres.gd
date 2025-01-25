3770ead4d0e7e5db93f29f63332b5b88faed8848
lucenstuff <lucentiniagustin@hotmail.com>
2025-01-25 13:15:23 -0300

    Update player mechanics and add hamster ball model

--- player/scripts/player.tres.gd
+++ player/scripts/player.tres.gd
@@ -1,12 +1,24 @@
 extends RigidBody3D
 
-@export var move_speed := 10.0
-@export var jump_force := 5.0
+@export var move_speed := 5
+@export var jump_force := 8.0
+@export var camera_rotation_speed := 1.1 
+@export var max_velocity := 12
 
-@onready var camera_rig: Node3D = $"../CameraRig"
+@onready var camera_rig: Node3D = $CameraRig
+
+func _ready() -> void:
+	camera_rig.top_level = true
+	camera_rig.global_position = global_position
 
 func _physics_process(delta: float) -> void:
-	# Get camera's forward and right vectors, but ignore Y component for ground movement
+	camera_rig.global_position = global_position
+	
+	if Input.is_action_pressed("move_left"):  
+		camera_rig.rotate_y(deg_to_rad(camera_rotation_speed)) 
+	if Input.is_action_pressed("move_right"): 
+		camera_rig.rotate_y(deg_to_rad(-camera_rotation_speed))  
+	
 	var camera_basis = camera_rig.global_transform.basis
 	var forward = -camera_basis.z
 	forward.y = 0
@@ -16,17 +28,17 @@ func _physics_process(delta: float) -> void:
 	right.y = 0
 	right = right.normalized()
 	
-	# Get input direction
 	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
 	
-	# Calculate movement direction relative to camera
 	var movement = (forward * -input_dir.y + right * input_dir.x).normalized()
 	
 	if movement:
-		# Apply force in the movement direction
 		apply_central_force(movement * move_speed)
 	
-	# Handle jump
+	var current_velocity = linear_velocity
+	if current_velocity.length() > max_velocity:
+		linear_velocity = current_velocity.normalized() * max_velocity
+	
 	if Input.is_action_just_pressed("jump") and is_on_floor_raycast():
 		apply_central_impulse(Vector3.UP * jump_force)
 
