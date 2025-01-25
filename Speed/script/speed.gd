189d311844fa72f005a77c40395673ce3006b26a
Julián Giamportone <chulogiamportone@gmail.com>
2025-01-25 15:37:50 -0300

    2players

---
+++ Speed/script/speed.gd
@@ -0,0 +1,13 @@
+class_name Speed extends Area3D
+
+@export var boost:int=20
+
+
+func _on_body_entered(body: Node3D) -> void:
+	if body is RigidBody3D:
+		body.move_speed*=boost
+
+
+func _on_body_exited(body: Node3D) -> void:
+	if body is RigidBody3D:
+		body.move_speed=body.real_speed
